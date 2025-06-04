de51d485ce8de421824bb3bdacb226feca7c1c9c
oumayma12 <ben@spls.ae>
2025-06-04 15:12:26 +0400

    fix ...

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,7 +5,7 @@ import { lusitana } from '@/app/ui/fonts';
 import { fetchRevenue } from '../lib/data';
  
 export default async function Page() {
-    const revenue = await fetchRevenue();
+    // const revenue = await fetchRevenue();
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
